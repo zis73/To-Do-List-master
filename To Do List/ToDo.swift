e0776ef5be75bb5a249f79c3271253ca53267db8
zis <desert73eagle@gmail.com>
2018-10-23 12:47:11 +0300

    Initial Commit

---
+++ To Do List/ToDo.swift
@@ -0,0 +1,29 @@
+//
+//  ToDo.swift
+//  To Do List
+//
+//  Created by Denis Bystruev on 23/10/2018.
+//  Copyright © 2018 Denis Bystruev. All rights reserved.
+//
+
+import Foundation
+
+struct ToDo {
+    var title: String
+    var isComplete: Bool
+    var dueDate: Date
+    var notes: String
+    
+    static func loadToDos() -> [ToDo]? {
+        return nil
+    }
+    
+    static func loadSampleToDos() -> [ToDo] {
+        return [
+            ToDo(title: "Купить хлеб", isComplete: false, dueDate: Date(), notes: "Бородинский"),
+            ToDo(title: "Заказать торт", isComplete: false, dueDate: Date(), notes: "Заметка 2"),
+            ToDo(title: "Сдать вещи в химчистку", isComplete: false, dueDate: Date(), notes: "Заметка 3"),
+            ToDo(title: "Помыть машину", isComplete: false, dueDate: Date(), notes: "Заметка 4"),
+        ]
+    }
+}
